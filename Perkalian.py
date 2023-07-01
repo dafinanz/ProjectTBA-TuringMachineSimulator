5f8c60a442c81da2fa34a55152f773ed907a3a97
asyahra19 <asyahrara02@student.uns.ac.id>
2023-07-01 12:34:07 +0700

    editing in progress...

--- Perkalian.py
+++ Perkalian.py
@@ -1,4 +1,4 @@
-def multiplicationMode(self):
+def perkalian(self):
     self.states = {'q0', 'q1', 'q2', 'q3', 'q4', 'q5', 'q6', 'q7', 'q8', 'q9', 
                    'q10','q11', 'q12', 'q13', 'q14', 'q15', 'q16', 'q17'}
     self.symbols = {'+', '-', '0', '1', 'B', 'X', 'Y', 'Z'}
